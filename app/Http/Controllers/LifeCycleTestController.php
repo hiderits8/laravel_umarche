72f6d69255f624519d7d765dd8c37547bb9e4d26
hiderits8 <wataru.ts8.hideri@gmail.com>
2024-04-17 18:39:39 +0900

    サービスコンテナ

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -14,8 +14,32 @@ class LifeCycleTestController extends Controller
 
         $test = app()->make('lifeCycleTest');
 
+        //サービスコンテナなしのパターン
+        $message = new Message();
+        $sample = new Sample($message);
+        $sample->run();
+
         dd($test, app());
     }
+}
 
-    //
+class Sample
+{
+    public $message;
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+    public function run()
+    {
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send()
+    {
+        echo 'メッセージ表示';
+    }
 }
